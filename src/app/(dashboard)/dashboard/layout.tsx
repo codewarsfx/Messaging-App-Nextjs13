c5c945d699ef6bd7957199fe87b952cca90296bb
codewarsfx <chiderainnocent001@gmail.com>
2023-06-20 15:22:26 +0100

    feat:display incoming friend requests count

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,7 +1,11 @@
+import FriendRequestsSidebarOption from "@/components/FriendRequestsSidebarOption";
+import SignOutButton from "@/components/SignOutButton";
 import { Icon, Icons } from "@/components/icons";
+import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { getSession } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { FC, ReactNode } from "react";
@@ -30,9 +34,10 @@ const Layout = async ({ children }: Layoutprops) => {
 	const session = await getServerSession(authOptions);
 
 	if (!session) notFound();
+	const initialCount = (await fetchRedis("smembers", `user:${session.user.id}:incoming_friend_requests`) as User[]).length;
 	return (
 		<div className='w-full flex h-screen'>
-			<div className='flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
+			<div className='flex h-full w-full max-w-sm  grow  flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
 				<Link className='flex h-16 shrink-0 items-center' href='/dashboard'>
 					<Icons.Logo className='h-8 w-auto text-indigo-200' />
 				</Link>
@@ -43,7 +48,7 @@ const Layout = async ({ children }: Layoutprops) => {
 					<ul role='list' className='flex flex-1 flex-col gap-y-7 '>
 						<li>//chats</li>
 						<li>
-							<div className='text-sx font-semibold leading-6 text-gray-400'>
+							<div className='text-xs font-semibold leading-6 text-gray-400'>
 								Overview
 							</div>
 							<ul role='list' className='-mx-2 mt-2 space-y-1'>
@@ -57,15 +62,40 @@ const Layout = async ({ children }: Layoutprops) => {
 											>
 												<span className='text-gray-400 border-gray-200 bg-white group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium'>
 													<Icon className='h-4 w-4' />
-                                                </span>
-                                                <span className="truncate">
-                                                    {sidebarOption.name}
-                                                </span>
+												</span>
+												<span className='truncate'>{sidebarOption.name}</span>
 											</Link>
 										</li>
 									);
 								})}
 							</ul>
+						</li>
+						<li>
+							<FriendRequestsSidebarOption
+								sessionId={session.user.id}
+								initialCount={initialCount}
+							/>
+						</li>
+						<li className='-mx-6 mt-auto flex items-center'>
+							<div className='flex gap-x-4 items-center px-6 py-3 flex-1 text-sm font-semibold leading-6 text-gray-900 '>
+								<div className='relative h-8 w-8 bg-gray-50'>
+									<Image
+										fill
+										referrerPolicy='no-referrer'
+										className='rounded-full'
+										alt='your profile picture'
+										src={session.user.image || ""}
+									/>
+								</div>
+								<span className='sr-only'>Your Profile</span>
+								<div className='flex flex-col'>
+									<span aria-hidden='true'>{session.user.name}</span>
+									<span className='text-xs text-zinc-400' aria-hidden='true'>
+										{session.user.email}
+									</span>
+								</div>
+							</div>
+							<SignOutButton className='h-full  aspect-square' />
 						</li>
 					</ul>
 				</nav>
